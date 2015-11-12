72f98b4a91fc502b70a175780f329c6599fa348d
David A. Ortega <dortega@me.com>
2015-11-12 15:30:38 -0800

    Initial commit

---
+++ src/main/java/com/ornaapplabs/hangoutsclient/HangoutsClientApplication.java
@@ -0,0 +1,12 @@
+package com.ornaapplabs.hangoutsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangoutsClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangoutsClientApplication.class, args);
+    }
+}
